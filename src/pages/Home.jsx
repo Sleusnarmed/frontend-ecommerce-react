e44be6decf53904c72ff6fed826ded339e321cf8
Jose Salazar <JMSalazar.macg@hotmail.com>
2023-05-11 23:52:23 -0600

    logo-insert-homepages-init

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import deliveryService from '@/assets/delivery-photo.png'
+import logo from '@/assets/SHIPPED-LOGO.png'
 
 const Home = () => {
   return (
-    <div>Shipped</div>
+    <>
+      <main className='home'>
+        <div className='home__welcome'>
+          <div className='welcome__container'>
+            <article className='welcome__article'>
+              <p>Welcome to</p>
+              <h1>SHIPPED</h1>
+            </article>
+            <button className='welcome__btn-shop'>
+              GO SHOP!
+            </button>
+          </div>
+          <div>
+            <img src={deliveryService} alt='' className='welcome__img' />
+          </div>
+        </div>
+        <div className='home__slogan'>
+          <article className='slogan__subtitle'>
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing.</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem reiciendis eos quibusdam!</p>
+          </article>
+        </div>
+      </main>
+      <footer className='hidden'>
+        <div>QuickLinks</div>
+        <div>LOGO</div>
+        <div>SocialMedia</div>
+      </footer>
+    </>
   )
 }
 
